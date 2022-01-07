0f41a0d9d6795df313c507ecf8657058a80f13b2
WinnieCWu <winniechn7@gmail.com>
2022-01-06 22:39:25 -0800

    use var to store user input

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,23 @@
-window.alert("This is an alert! Javascript is running!");
+//this creates a function named "fight"
+function fight(){
+    window.alert("The fight has begun!");
+}
+
+//need this to call the function
+///fight();
+
+//window.prompt("What is your robot's name?");
+//to capture the data, create a variable 
+var playerName = window.prompt("What is your robot's name?");
+//Note the lack of quotation marks around playerName
+///window.alert(playerName);
+//replace with
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+//this will do math and log 20
+console.log(10+10);
+//what is this?
+console.log("Our robot's name is " +playerName);
+
+var name ="your name";
+console.log(name);
