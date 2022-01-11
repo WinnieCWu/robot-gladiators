76bcbeda88bd9645f8c8a8bd1afe7c9dc8025a0c
WinnieCWu <winniechn7@gmail.com>
2022-01-11 15:35:53 -0800

    updated shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,26 +11,42 @@ var randomNumber = function(min, max) {
   return value;
 };
 
-// fight function (now with parameter for enemy's object holding name, health, and attack values)
-var fight = function(enemy) {
-  while (playerInfo.health > 0 && enemy.health > 0) {
-    // ask player if they'd like to fight or run
-    var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+var fightOrSkip = function() {
+  //ask player if they'd like to fight or skip using this fxn
+  var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.')
+  // Conditional Recursive Function Call
+  //if the `promptFight` is not a valid value, then execute the following statement in alert
+  if (promptFight === "" || promptFight === null) {
+    window.alert("You need to provide a valid answer! Please try again.");
+    // use return to call it again and stop the rest of this function from running
+    return fightOrSkip();
+  }
 
-    // if player picks "skip" confirm and then stop the loop
-    if (promptFight === "skip" || promptFight === "SKIP") {
-      // confirm player wants to skip
+  // convert promptFight to all lowercase so we can check with less options
+  promptFight = promptFight.toLowerCase();
+
+  if (promptFight === "skip") /* or if(!promptFight) */ {
+      //confirm player wants to skip
       var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-
       // if yes (true), leave fight
       if (confirmSkip) {
         window.alert(playerInfo.name + ' has decided to skip this fight. Goodbye!');
         // subtract money from playerInfo.money for skipping
         playerInfo.money = Math.max(0, playerInfo.money - 10);
-        console.log("playerInfo.money", playerInfo.money)
-        break;
+        //return true if player wants to leave
+        return true;
       }
     }
+    return false;
+}
+
+// fight function (now with parameter for enemy's object holding name, health, and attack values)
+var fight = function(enemy) {
+  while (playerInfo.health > 0 && enemy.health > 0) {
+    // ask player if they'd like to fight or run
+    if (fightOrSkip()) {
+      break;
+    }; //-- replaced the code with this function call amd add fight logic
 
 //Subtract the value of `playerAttack` from the value of `enemyHealth
 //if player chooses to fight, then fight
@@ -116,7 +132,6 @@ var startGame = function() {
         break;
       }
     }
-  
     // after loop ends, we are either out of player.health or enemies to fight, so run the endGame function
     endGame();
   };
@@ -147,28 +162,24 @@ var startGame = function() {
 var shop = function() {
   // ask player what they'd like to do
   var shopOptionPrompt = window.prompt(
-    'Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one "REFILL", "UPGRADE", or "LEAVE" to make a choice.'
+    'Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE.'
   );
-
+    shopOptionPrompt = parseInt(shopOptionPrompt);
   // use switch case to carry out action
   switch (shopOptionPrompt) {
-    case 'REFILL':
-    case 'refill':
+    case 1:
       playerInfo.refillHealth();
       break;
-    case 'UPGRADE':
-    case 'upgrade':
+    case 2:
       playerInfo.upgradeAttack();
       break;
-    case 'LEAVE':
-    case 'leave':
+    case 3:
       window.alert('Leaving the store.');
 
       // do nothing, so function will end
       break;
     default:
       window.alert('You did not pick a valid option. Try again.');
-
       // call shop() again to force player to pick a valid option
       shop();
       break;
@@ -178,10 +189,10 @@ var shop = function() {
 /* END GAME FUNCTIONS */
 
 //function to set name
-var getPlayerName = function(){
+var getPlayerName = function() {
   var name = "";
   //ADD LOOP HERE WITH PROMPT AND CONDITIONS
-  while (name === "" || name === null){
+  while (name === "" || name === null) {
     name = prompt("What is your robot's name?");
   }
   console.log("Your robot's name is " + name);
@@ -238,11 +249,7 @@ var playerInfo = {
       attack: randomNumber(10, 14)
     }
   ];
-  
-  console.log(enemyInfo);
-  console.log(enemyInfo[0]);
-  console.log(enemyInfo[0].name);
-  console.log(enemyInfo[0]['attack']);
+
 
   /* END GAME INFORMATION / VARIABLES */
   
